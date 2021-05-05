ea593b421a6fa8c615c04507540fdf5254230c4d
GiovanniHM <giovannihernandezm26@gmail.com>
2021-05-05 13:52:19 -0500

    class 123

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = "";
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  activarRegion(region: string){
+
+    if(region === this.regionActiva)
+      return;
+    this.regionActiva = region;
+    this.paises = [];
+    this.getPaises(region)
+
+
+  }
+
+  getClaseCSS(region: string){
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary'
+  }
+
+  getPaises(region: string){
+
+    this.paisService.buscarPais(region, 'region').subscribe(
+      (resp) => {
+        this.paises = resp;
+      }
+    );
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
@@ -8,13 +9,26 @@ import { Country } from '../interfaces/pais.interface';
 })
 export class PaisService {
 
-  private apiUrl: string = "https://restcountries.eu/rest/v2"
+  private apiUrl: string = "https://restcountries.eu/rest/v2";
+
+
+  get httpParams(){
+    return new HttpParams().set('fields', 'name;capital;alpha2Code;flag;population');
+  }
+
 
   constructor( private http: HttpClient) { }
 
   buscarPais(termino: string, type: string): Observable<Country[]>{
+
+
+
+
     const url = `${this.apiUrl}/${type}/${termino}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.httpParams})
+            .pipe(
+              tap(console.log)
+            );
   }
 
   getPaisPorAlpha(id:string):Observable<Country>{
